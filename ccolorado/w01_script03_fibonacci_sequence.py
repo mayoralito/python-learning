ecade88bf8e668357d8eff8592735755b5282cd6
Carlos Colorado <carlos.colorado@gmail.com>
2014-10-06 01:07:06 -0500

    ading cli arguments

--- ccolorado/w01_script03_fibonacci_sequence.py
+++ ccolorado/w01_script03_fibonacci_sequence.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
-
+import sys
 
 def calculate_fibonacci( number ):
     if number == 1  or number == 2:
         return 1
     return calculate_fibonacci(number - 1 ) + calculate_fibonacci( number - 2)
 
+
+number = int(sys.argv[1])
+if not isinstance( number, int):
+  sys.exit(1)
+
+series = []
+for n in range (1, number):
+  series.append( calculate_fibonacci(n) )
+
+print series
